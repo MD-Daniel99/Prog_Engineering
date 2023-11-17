5adf3d2a4c8af74301f29cb126355729c05e9f54
MD-Daniel99 <danmir1998@gmail.com>
2023-11-17 21:59:09 +0300

    Delete dublicae

--- assignment_1.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-transcriber = pipeline("automatic-speech-recognition", model = "openai/whisper-large-v2")
-res = transcriber("https://ia801603.us.archive.org/35/items/DoNotGoGentleIntoThatGoodNight/gentle_64kb.mp3")
-
-print(res)
