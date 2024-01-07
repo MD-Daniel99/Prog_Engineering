e1caf66c78832eb3a563e7675f4465988135a3df
MD-Daniel99 <danmir1998@gmail.com>
2024-01-07 18:16:58 +0300

    fff

--- Assignment_2/test_app.py
+++ Assignment_2/test_app.py
@@ -1,22 +1,32 @@
 import requests
-import json
+import pytest
+from main import model_loading, get_url, processing, printing
 
 BASE_URL = "https://astronaut-snowboarding-across-theuniverse.streamlit.app"
+
+# Тесты
+def test_root_endpoint():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200
+
+def test_http_status_code():
+    data = {"image_url": "example.jpg"}
+    response = requests.post(f"{BASE_URL}/process_image", json=data)
+    assert response.status_code == 200
 
 def test_response_content():
     data = {"image_url": "example.jpg"}
     response = requests.post(f"{BASE_URL}/process_image", json=data)
-    
-    # Проверяем, что ответ содержит JSON-данные
     assert "description" in response.json()
 
-# Удаляем тест, так как теперь у вас фиксированный URL
-# def test_valid_image_url():
-#     assert get_url() is not None
+def test_app_availability():
+    response = requests.get(f"{BASE_URL}/process_image")
+    assert response.status_code == 200
+
+def test_model_loading():
+    assert model_loading() is not None
 
 def test_image_recognition_result():
     data = {"image_url": "https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp"}
     response = requests.post(f"{BASE_URL}/process_image", json=data)
-    
-    # Проверяем, что "Image described:" есть в тексте ответа
     assert "Image described:" in response.text
