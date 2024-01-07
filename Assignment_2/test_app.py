765c7e493a71a5ac708286e592ca5dddb3fd69a5
MD-Daniel99 <danmir1998@gmail.com>
2024-01-07 18:47:11 +0300

    fuaf

--- Assignment_2/test_app.py
+++ Assignment_2/test_app.py
@@ -9,20 +9,21 @@ def test_model_loading():
 
 # Тест 2: Проверка, что url-картинки получено
 def test_get_url():
-    with st._inject_button("Click to start"):
+    with st.button("Click to start"):
         st.text_input("Enter picture URL:", value="https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp")
     get_image_url = get_url()
     assert get_image_url == "https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp"
 
 # Тест 3: Проверка вывода результата работы модели
 def test_processing_and_printing():
-    with st._inject_button("Click to start"):
+    with st.button("Click to start"):
         st.text_input("Enter picture URL:", value="https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp")
     pred = model_loading()
     get_image_url = get_url()
-    with st._inject_button("Click to start"):
+    res = None
+    with st.button("Click to start"):
         res = pred(get_image_url)
-    with st._inject_button("Click to start"):
+    with st.button("Click to start"):
         printing(res)
     assert st.get_last_widget_value() == "Image described:\n" + res
 
