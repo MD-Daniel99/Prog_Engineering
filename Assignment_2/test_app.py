2da5bc77ee35d6f66d09eb4e1b901d0aab7fa0f2
MD-Daniel99 <danmir1998@gmail.com>
2024-01-07 17:01:49 +0300

    AAA

--- Assignment_2/test_app.py
+++ Assignment_2/test_app.py
@@ -1,11 +1,15 @@
 # test_app.py
 from assignment_2 import model_loading, get_url
+from fastapi.testclient import TestClient
+from assignment_2 import app
+
+client = TestClient(app)
 
 def test_model_loading():
     model = model_loading()
     assert model is not None  
 
 def test_get_url():
-    assert is_valid_image_url("https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp") 
+    assert get_url("https://www.quickanddirtytips.com/wp-content/uploads/2019/12/astronaut-jpg.webp") 
 
 
